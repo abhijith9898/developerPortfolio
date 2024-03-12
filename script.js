b3d363cd3671bb02c2f088b6dbc51b42d78935fb
Abhijith Rajeev <abhijith.rajeev9898@gmail.com>
2024-03-12 03:42:11 -0400

    mobile navbar and profile section updated

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleClick() {
-    let id = document.getElementById("nav-link");
-    if(id.style.display === 'block') {
-        id.style.display = 'none';
-    } else {
-        id.style.display = 'block';
-    }
-}
